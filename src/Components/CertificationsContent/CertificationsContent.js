95b2f744f6eba3f14c5aabfa20ee239965593c66
JGMEYER <meyer.justian@gmail.com>
2021-03-21 05:11:30 -0400

    Fix propType warning

--- src/Components/CertificationsContent/CertificationsContent.js
+++ src/Components/CertificationsContent/CertificationsContent.js
@@ -8,46 +8,46 @@ function CertificationsContent() {
             <FadeInSection>
                 <CertificationItem
                     certName="Front-End Engineer Career Path"
-                    hours="350"
+                    hours={350}
                     certLink="certifications/front-end-engineer-career-path.pdf"
                 ></CertificationItem>
             </FadeInSection>
             <FadeInSection>
                 <CertificationItem
                     certName="Learn HTML"
-                    hours="9"
+                    hours={9}
                     certLink="certifications/learn-html-course.pdf"
                 ></CertificationItem>
             </FadeInSection>
             <FadeInSection>
                 <CertificationItem
                     certName="Learn CSS"
-                    hours="20"
+                    hours={20}
                     certLink="certifications/learn-css-course.pdf"
                 ></CertificationItem>
             </FadeInSection>
             <FadeInSection>
                 <CertificationItem
                     certName="Learn Javascript"
-                    hours="30"
+                    hours={30}
                     certLink="certifications/learn-javascript-course.pdf"
                 ></CertificationItem>
             </FadeInSection>
             <FadeInSection>
                 <CertificationItem
                     certName="Building Interactive Javascript Websites"
-                    hours="7"
+                    hours={7}
                     certLink="certifications/building-interactive-javascript-websites-course.pdf"
                 ></CertificationItem>
             </FadeInSection>
             <FadeInSection>
                 <CertificationItem
                     certName="Learn React"
-                    hours="20"
+                    hours={20}
                     certLink="certifications/learn-react-course.pdf"
                 ></CertificationItem>
             </FadeInSection>
-        </div>
+        </div >
     )
 }
 
