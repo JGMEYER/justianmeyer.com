9c7ede70769c5a9bbf37486b68c908069fc6edb0
JGMEYER <meyer.justian@gmail.com>
2021-03-21 15:43:45 -0400

    Attempt to fix certifications pdf paths in prod

--- src/Components/CertificationsContent/CertificationsContent.js
+++ src/Components/CertificationsContent/CertificationsContent.js
@@ -9,42 +9,42 @@ function CertificationsContent() {
                 <CertificationItem
                     certName="Front-End Engineer Career Path"
                     hours={350}
-                    certLink="certifications/front-end-engineer-career-path.pdf"
+                    certLink="/certifications/front-end-engineer-career-path.pdf"
                 ></CertificationItem>
             </FadeInSection>
             <FadeInSection>
                 <CertificationItem
                     certName="Learn HTML"
                     hours={9}
-                    certLink="certifications/learn-html-course.pdf"
+                    certLink="/certifications/learn-html-course.pdf"
                 ></CertificationItem>
             </FadeInSection>
             <FadeInSection>
                 <CertificationItem
                     certName="Learn CSS"
                     hours={20}
-                    certLink="certifications/learn-css-course.pdf"
+                    certLink="/certifications/learn-css-course.pdf"
                 ></CertificationItem>
             </FadeInSection>
             <FadeInSection>
                 <CertificationItem
                     certName="Learn Javascript"
                     hours={30}
-                    certLink="certifications/learn-javascript-course.pdf"
+                    certLink="/certifications/learn-javascript-course.pdf"
                 ></CertificationItem>
             </FadeInSection>
             <FadeInSection>
                 <CertificationItem
                     certName="Building Interactive Javascript Websites"
                     hours={7}
-                    certLink="certifications/building-interactive-javascript-websites-course.pdf"
+                    certLink="/certifications/building-interactive-javascript-websites-course.pdf"
                 ></CertificationItem>
             </FadeInSection>
             <FadeInSection>
                 <CertificationItem
                     certName="Learn React"
                     hours={20}
-                    certLink="certifications/learn-react-course.pdf"
+                    certLink="/certifications/learn-react-course.pdf"
                 ></CertificationItem>
             </FadeInSection>
         </div >
