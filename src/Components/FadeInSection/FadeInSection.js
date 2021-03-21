bf58a8982bd307dd54d87c36fd2f9849fec8c9cf
JGMEYER <meyer.justian@gmail.com>
2021-03-21 05:16:34 -0400

    Fix domRef.current reference warning

--- src/Components/FadeInSection/FadeInSection.js
+++ src/Components/FadeInSection/FadeInSection.js
@@ -6,11 +6,12 @@ function FadeInSection(props) {
     const [isVisible, setVisible] = React.useState(true);
     const domRef = React.useRef();
     React.useEffect(() => {
+        const current = domRef.current;
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => setVisible(entry.isIntersecting));
         });
-        observer.observe(domRef.current);
-        return () => observer.unobserve(domRef.current);
+        observer.observe(current);
+        return () => observer.unobserve(current);
     }, []);
     return (
         <div
