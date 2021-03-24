6c460cf464080f9a4ebc6bef5e74da70669726b9
JGMEYER <meyer.justian@gmail.com>
2021-03-24 01:24:22 -0400

    Add additional "about" information

--- src/Components/AboutContent/AboutContent.js
+++ src/Components/AboutContent/AboutContent.js
@@ -1,10 +1,15 @@
 import FadeInSection from "../FadeInSection/FadeInSection";
 
+import './AboutContent.css';
+
 function AboutContent() {
     return (
-        <FadeInSection>
-            <p>Hi, I'm Justian, a frontend engineer with backend experience at top Silicon Valley companies.</p>
-        </FadeInSection>
+        <div className="about-content-wrapper">
+            <FadeInSection>
+                <p>Hi, I'm <em>Justian</em>, a software engineer with backend experience at top <em>Silicon Valley</em> companies.</p>
+                <p>In an effort to continue growing and expanding my skills, I've been teaching myself <em>frontend</em> development, primarily <em>Reactjs</em>, and am open to employment opportunities.</p>
+            </FadeInSection >
+        </div>
     );
 }
 
