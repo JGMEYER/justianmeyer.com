a9e391b0a7301b5c6eb4878cf14f160cd3344143
JGMEYER <meyer.justian@gmail.com>
2021-04-01 00:02:55 -0400

    Remove icon badges and replace with icon. Remove text with intention of adding on hover

--- src/Components/PortfolioContent/PortfolioContent.js
+++ src/Components/PortfolioContent/PortfolioContent.js
@@ -1,6 +1,6 @@
 import './PortfolioContent.css';
 import FadeInSection from '../FadeInSection/FadeInSection';
-import IconBadge from '../IconBadge/IconBadge';
+// import IconBadge from '../IconBadge/IconBadge';
 import iconAmplify from '../../images/icons/amplify-white.svg';
 import iconReact from '../../images/icons/react-white.svg';
 
@@ -9,29 +9,36 @@ function PortfolioContent() {
         <div className="portfolio-content-container">
             <FadeInSection>
                 <a className="portfolio-item-container" href="/">
-                    <h4>Personal Website</h4>
-                    <p>Website for hosting experience and portfolio items.</p>
-                    <div className="technologies-container">
-                        <IconBadge icon={iconReact} iconBgColor='#6AD1FB' name='React'></IconBadge>
-                        <IconBadge icon={iconAmplify} iconBgColor='#FF9100' name='AWS Amplify'></IconBadge>
+                    <div className="portfolio-item-container-inner">
+                        <h4>Personal Website</h4>
+                        <div className="technologies-container">
+                            {/* <IconBadge icon={iconReact} iconBgColor='#6AD1FB' name='React'></IconBadge>
+                            <IconBadge icon={iconAmplify} iconBgColor='#FF9100' name='AWS Amplify'></IconBadge> */}
+                            <img className="technology-icon" src={iconReact} title="ReactJS"></img>
+                            <img className="technology-icon" src={iconAmplify} title="AWS Amplify"></img>
+                        </div>
                     </div>
                 </a>
             </FadeInSection>
             <FadeInSection>
                 <a className="portfolio-item-container" href="https://jgmeyer.github.io/js-traffic-sim/" target="_blank" rel="noopener noreferrer">
-                    <h4>Traffic Simulation</h4>
-                    <p>Fun road builder and traffic simulator.</p>
-                    <div className="technologies-container">
-                        <IconBadge icon={iconReact} iconBgColor='#6AD1FB' name='React'></IconBadge>
+                    <div className="portfolio-item-container-inner">
+                        <h4>Traffic Simulation</h4>
+                        <div className="technologies-container">
+                            <img className="technology-icon" src={iconReact} title="ReactJS"></img>
+                            {/* <IconBadge icon={iconReact} iconBgColor='#6AD1FB' name='React'></IconBadge> */}
+                        </div>
                     </div>
                 </a>
             </FadeInSection>
             <FadeInSection>
                 <a className="portfolio-item-container" href="https://jgmeyer.github.io/js-chess/" target="_blank" rel="noopener noreferrer">
-                    <h4>Chess Game</h4>
-                    <p>Classic game of chess for the browser.</p>
-                    <div className="technologies-container">
-                        <IconBadge icon={iconReact} iconBgColor='#6AD1FB' name='React'></IconBadge>
+                    <div className="portfolio-item-container-inner">
+                        <h4>Chess Game</h4>
+                        <div className="technologies-container">
+                            <img className="technology-icon" src={iconReact} title="ReactJS"></img>
+                            {/* <IconBadge icon={iconReact} iconBgColor='#6AD1FB' name='React'></IconBadge> */}
+                        </div>
                     </div>
                 </a>
             </FadeInSection>
