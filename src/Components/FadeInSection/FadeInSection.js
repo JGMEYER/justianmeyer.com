9bf0df262727140e3c6b7dbb1dc20b9cdd63f4ca
JGMEYER <meyer.justian@gmail.com>
2021-03-31 22:17:09 -0400

    FadeInSection animates on load and now translates

--- src/Components/FadeInSection/FadeInSection.js
+++ src/Components/FadeInSection/FadeInSection.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './FadeInSection.css';
 
 function FadeInSection(props) {
-    const [isVisible, setVisible] = React.useState(true);
+    const [isVisible, setVisible] = React.useState(false);
     const domRef = React.useRef();
     React.useEffect(() => {
         const current = domRef.current;
